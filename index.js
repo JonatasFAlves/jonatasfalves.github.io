88ccc5a4e8ac600a6e4187355f9ed3a4ded2a099
JonatasFAlves <alves.jnts@gmail.com>
2018-01-27 13:03:50 -0300

    Refactoring page

---
+++ index.js
@@ -0,0 +1,13 @@
+var images = [], x = -1;
+images[0] = "assets/images/me2.png";
+images[1] = "assets/images/me3.png";
+images[2] = "assets/images/me1.png";
+
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("img").src = images[x];
+}
+
+function startTimer() {
+    setInterval(displayNextImage, 7500);
+}
