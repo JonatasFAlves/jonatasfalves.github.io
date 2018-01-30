ba1bd37119a6d779f606dc6017b4e4f294374e64
JonatasFAlves <alves.jnts@gmail.com>
2018-01-30 02:10:30 -0300

    projects: Outsourced javascript code to projects.js file

---
+++ projects/projects.js
@@ -0,0 +1,21 @@
+const mySiema = new Siema({
+    duration: 300,
+    onInit: changeCarouselInfo,
+    onChange: changeCarouselInfo
+});
+
+function changeCarouselInfo() {
+    if(this.currentSlide <= 1){
+        document.querySelector('.info').innerHTML = 'Trying to solve a school management problem, the SALAS software help faculties manage it\'s professors and classrooms. Working with Java and MySQL';
+        document.querySelector('.tech-info').innerHTML = 'Working with Java and MySQL';
+    } else if (this.currentSlide <= 3) {
+        document.querySelector('.info').innerHTML = 'The MeuColetivo app helps public transportation users be heard.';
+        document.querySelector('.tech-info').innerHTML = 'Working with Web development';
+    } else {
+        document.querySelector('.info').innerHTML = 'In my spare time, i make some css replications too, mostly from Dribbble. Working with web technologies.';
+        document.querySelector('.tech-info').innerHTML = 'Working with Html, CSS';
+    }
+}
+
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
