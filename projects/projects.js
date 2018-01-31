2fdb2c26c3f1256096264dea2ad4131573b532de
JonatasFAlves <alves.jnts@gmail.com>
2018-01-31 00:53:02 -0300

    projects: Changed carousel text

--- projects/projects.js
+++ projects/projects.js
@@ -12,8 +12,8 @@ function changeCarouselInfo() {
         document.querySelector('.info').innerHTML = 'The MeuColetivo app helps public transportation users be heard.';
         document.querySelector('.tech-info').innerHTML = 'Working with Web development and Bootstrap';
     } else {
-        document.querySelector('.info').innerHTML = 'In my spare time, i make some css replications too, mostly from Dribbble. Working with web technologies.';
-        document.querySelector('.tech-info').innerHTML = 'Working with Html, CSS';
+        document.querySelector('.info').innerHTML = 'In my spare time, i make some css replications. Mostly from Dribbble.';
+        document.querySelector('.tech-info').innerHTML = 'Working with HTML, CSS';
     }
 }
 
