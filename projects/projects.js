fdede5f2d85226db64af56489a7569bd7bcba927
JonatasFAlves <alves.jnts@gmail.com>
2018-02-02 23:29:25 -0300

    projects: Fix typos

--- projects/projects.js
+++ projects/projects.js
@@ -6,10 +6,10 @@ const mySiema = new Siema({
 
 function changeCarouselInfo() {
     if(this.currentSlide <= 1){
-        document.querySelector('.info').innerHTML = 'Trying to solve a school management problem, the SALAS software help faculties manage it\'s professors and classrooms. Working with Java and MySQL';
+        document.querySelector('.info').innerHTML = 'Solving a school management problem, the SALAS software help faculties manage professors and classrooms.';
         document.querySelector('.tech-info').innerHTML = 'Working with Java and MySQL';
     } else if (this.currentSlide <= 3) {
-        document.querySelector('.info').innerHTML = 'The MeuColetivo app helps public transportation users be heard.';
+        document.querySelector('.info').innerHTML = 'The MeuColetivo app help public transportation users be heard.';
         document.querySelector('.tech-info').innerHTML = 'Working with Web development and Bootstrap';
     } else {
         document.querySelector('.info').innerHTML = 'In my spare time, i make some css replications. Mostly from Dribbble.';
